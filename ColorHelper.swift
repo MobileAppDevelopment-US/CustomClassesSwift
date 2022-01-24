ca14d6368736190334f8cd173d61b2bb2111035b
Serik <serikklement@gmail.com>
2022-01-24 18:42:40 -0500

    added hex

--- ColorHelper.swift
+++ ColorHelper.swift
@@ -56,3 +56,27 @@ extension UIColor {
         return UIColor(patternImage: image)
     }
 }
+
+extension UIColor {
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+    convenience init?(hexaRGBA: String) {
+        var chars = Array(hexaRGBA.hasPrefix("#") ? hexaRGBA.dropFirst() : hexaRGBA[...])
+        switch chars.count {
+        case 3: chars = chars.flatMap { [$0, $0] }; fallthrough
+        case 6: chars.append(contentsOf: ["F", "F"])
+        case 8: break
+        default: return nil
+        }
+        self.init(red: .init(strtoul(String(chars[0 ... 1]), nil, 16)) / 255,
+                  green: .init(strtoul(String(chars[2 ... 3]), nil, 16)) / 255,
+                  blue: .init(strtoul(String(chars[4 ... 5]), nil, 16)) / 255,
+                  alpha: .init(strtoul(String(chars[6 ... 7]), nil, 16)) / 255)
+    }
+}
